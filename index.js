81e33741999138adf5c7b304fa1d9d34041b016c
Michelle Appiah <michappiah67@gmail.com>
2023-11-22 22:29:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
-app.use(cors());
-app.use(cors({ origin: 'https://inventory-application-zrr6.onrender.com' }));
+app.use(cors({ origin: 'https://inventory-application-zrr6.onrender.com/' }));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
