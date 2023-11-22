5dd7f747ce9fcd475f802c57cdb0d45732600d3e
Michelle Appiah <michappiah67@gmail.com>
2023-11-22 22:50:50 +0000

    Update UploadBook.jsx

--- client-side/src/dashboard/UploadBook.jsx
+++ client-side/src/dashboard/UploadBook.jsx
@@ -35,9 +35,9 @@ const UploadBook = () => {
     const bookObject = {
       bookTitle,authorName,imageUrl,category,bookDescription,bookPdfUrl
     }
-
+    const apiUrl = import.meta.env.VITE_API_BASE_URL;
     //send data to DB
-    fetch("http://localhost:5000/upload-book", {
+    fetch(`${apiUrl}/upload-book`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
