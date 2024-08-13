d088c4d8e568a7786b661aeaa26a0f0a85ff62ef
Coby232 <selormkn2003@gmail.com>
2024-08-13 16:57:22 +0000

    update on tasktalk

--- client-side/src/taskTalk/TaskTalk.jsx
+++ client-side/src/taskTalk/TaskTalk.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-// 
+//
 import { useState, useEffect, useContext, useRef } from "react";
 import {
   collection,
@@ -16,12 +16,15 @@ import {
 import { db } from "../firebase/firebase.config";
 import { AuthContext } from "../contexts/AuthProvider";
 import { useNavigate } from "react-router-dom";
+// the realtime database doesnt matter now I'm using cloud firestore but dont remove it 
+// for future use only
 import {
   getDatabase,
   ref,
   set,
   onDisconnect,
 } from "firebase/database";
+import { signOut } from "firebase/auth";
 import socketIO from "socket.io-client";
 
 const socket = socketIO.connect("http://localhost:5000");
@@ -41,6 +44,7 @@ function TaskTalk() {
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedChat, setSelectedChat] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [showChat, setShowChat] = useState(false);
 
   const dbRealtime = getDatabase();
 
@@ -147,16 +151,17 @@ function TaskTalk() {
   }, []);
 
   const handleSelectChat = async (selectedUser) => {
-    const chatId =
-      user.uid > selectedUser.uid
-        ? `${user.uid}_${selectedUser.uid}`
-        : `${selectedUser.uid}_${user.uid}`;
+    setShowChat(true);
+    const chatId = selectedUser.chatId
+      ? selectedUser.chatId
+      : `${user.uid}_${selectedUser.uid}`;
     const chatDocRef = doc(db, "TaskTalk", chatId);
     const chatSnapshot = await getDoc(chatDocRef);
 
     if (!chatSnapshot.exists()) {
       await setDoc(chatDocRef, {
         participants: [user.uid, selectedUser.uid],
+        chatId: selectedChat.id,
         lastMessage: "",
         timestamp: serverTimestamp(),
       });
@@ -180,10 +185,7 @@ function TaskTalk() {
     setMessages(chatMessages);
   };
 
-  useEffect(()=>{
-
-  })
-
+  useEffect(() => {});
 
   const handleTyping = () => {
     socket.emit("typing", `${userName} is typing...`);
@@ -191,7 +193,7 @@ function TaskTalk() {
 
   const handleSendMessage = async (e) => {
     e.preventDefault();
-    if (message.trim() && user && selectedChat) {
+    if (message.length > 0 && user && selectedChat) {
       const timestamp = new Date();
       const messageId = timestamp.getTime().toString();
 
@@ -200,8 +202,15 @@ function TaskTalk() {
         userId: user.uid,
         userName: userName,
         text: message,
+        // date
         timestamp,
         chatId: selectedChat.id,
+        participants: [
+          user.uid,
+          selectedChat.participants[1] !== user.uid
+            ? selectedChat.participants[1]
+            : selectedChat.participants[0],
+        ],
       };
 
       try {
@@ -217,10 +226,18 @@ function TaskTalk() {
         await updateDoc(chatDocRef, {
           lastMessage: chatMessage.text,
           timestamp: chatMessage.timestamp,
+          chatId: selectedChat.id,
+          participants: [
+            user.uid,
+            selectedChat.participants[1] !== user.uid
+              ? selectedChat.participants[1]
+              : selectedChat.participants[0],
+          ],
         });
 
         socket.emit("chatMessage", chatMessage);
         setMessage("");
+        handleSelectChat(selectedChat);
       } catch (error) {
         console.error("Error sending message:", error);
       }
@@ -230,14 +247,16 @@ function TaskTalk() {
   };
 
   const handleLeaveChat = () => {
-    localStorage.removeItem("userName");
-    navigate("/");
-    window.location.reload();
+    setShowChat(false);
   };
+
+  // useEffect(()=>{
+
+  // })
 
   return (
     <div className='flex h-screen p-20'>
-      <div className='sidebar p-4 w-1/4 border-r border-gray-300'>
+      <div className='sidebar sticky p-4 w-1/4 border-r border-gray-300'>
         <h2 className='text-xl mb-4'>TaskTalk</h2>
         <form onSubmit={handleSearch} className='mb-4'>
           <input
@@ -253,113 +272,153 @@ function TaskTalk() {
             {loading ? "Loading..." : "Search"}
           </button>
         </form>
-        <ul>
+        <ul className='flex flex-col gap-2'>
           Recent Chats
           {TaskTalk.map((chat) => (
             <li
               key={chat.id}
               onClick={() => handleSelectChat(chat)}
-              className={`p-2 cursor-pointer ${
+              className={`p-2 cursor-pointer hover:bg-slate-300 ${
                 selectedChat && selectedChat.id === chat.chatId
                   ? "bg-gray-200 rounded-lg"
                   : "border"
+              } ${
+                chat.participants[0] === chat.participants[1]
+                  ? "hidden"
+                  : "block"
               }`}>
               {chat.participants
-                .filter((participant) => participant !== user.uid)
-                .join(", ")}
-              <br />
-              <small>{chat.lastMessage}</small>
+                .filter((participantId) => participantId !== user.uid)
+                .map((participantId) => {
+                  const participant = users.find(
+                    (user) => user.uid === participantId
+                  );
+                  return (
+                    <div key={participantId} className=''>
+                      <p>
+                        {participant
+                          ? participant.name
+                          : "Unknown User"}
+                      </p>
+                      <small>
+                        recent message: {chat.lastMessage}
+                      </small>
+                    </div>
+                  );
+                })}
             </li>
           ))}
         </ul>
       </div>
-      <div className='chat-container p-4 w-3/4 flex flex-col'>
-        {selectedChat ? (
-          <>
-            <div className='messages flex-1 overflow-y-auto p-4'>
-              {/* Chat Dialog Header */}
-              <div className='chat-header mb-4'>
+      <div className=' p-4 w-3/4 flex flex-col'>
+        <div>
+          {showChat === true ? (
+            <div>
+              <div className='mb-2'>
                 <h2 className='text-2xl font-semibold'>
                   Chat Dialog
                 </h2>
               </div>
-              {/* Message List */}
-              <div className='message-list'>
-                {messages.map((msg) => (
-                  <div
-                    key={msg.id}
-                    className={`message p-3 mb-2 rounded-lg max-w-xs ${
-                      msg.userId === user.uid
-                        ? "bg-blue-100 self-end ml-auto text-right border border-blue-500"
-                        : "bg-gray-100 border border-gray-500"
-                    }`}>
-                    <div className='message-content'>
-                      <strong>{msg.userName}:</strong> {msg.text}
-                    </div>
-                    <div className='message-timestamp text-xs text-gray-500'>
-                      {new Date(
-                        msg.timestamp?.toDate()
-                      ).toLocaleTimeString()}
-                    </div>
-                  </div>
-                ))}
+              <div className=' flex-1 overflow-y-auto mb-2  border-2 border-dotted rounded-l p-4 h-[18.5em] scrollbar-none scrollbar-thumb-sky-700 scrollbar-track-sky-300'>
+                {/* Message List */}
+                <div className='message-list '>
+                  {messages.map((msg) => {
+                    const participant = users.find(
+                      (user) => user.uid === messages.userId
+                    );
+                    return (
+                      <div
+                        key={msg.id}
+                        className={`message p-2 mb-2 rounded-lg max-w-xs ${
+                          msg.userId === user.uid
+                            ? "bg-blue-100 self-end ml-auto text-right border border-blue-500"
+                            : "bg-gray-100 border border-gray-500"
+                        }`}>
+                        <div className='message-content'>
+                          <strong>
+                            {msg.userId === user.uid
+                              ? "You"
+                              : `{Participant}`}
+                          </strong>
+                          <p>{msg.text}</p>
+                        </div>
+                        <div className='message-timestamp text-xs text-gray-500'>
+                          {new Date(
+                              msg.timestamp?.toDate()
+                            ).toLocaleTimeString() +" "+new Date(
+                            msg.timestamp?.toDate()
+                          ).toDateString()
+                            }
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
                 <div ref={lastMessageRef} />
+                <div className='message__status mt-2'>
+                  <p className='text-gray-500 text-sm'>
+                    {typingStatus}
+                  </p>
+                </div>
+              </div>
+
+              <form
+                onSubmit={handleSendMessage}
+                className='chat-form flex'>
+                <input
+                  type='text'
+                  placeholder='Type a message'
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  onKeyDown={handleTyping}
+                  className='flex-1 p-2 border rounded'
+                />
+                <button
+                  type='submit'
+                  className='ml-2 p-2 bg-blue-500 text-white rounded'>
+                  Send
+                </button>
+              </form>
+
+              <div className='flex flex-col'>
+                <button
+                  onClick={handleLeaveChat}
+                  className='mt-2 bg-blue-500 text-white p-2 rounded'>
+                  Leave Chat
+                </button>
+                <button
+                  onClick={logout}
+                  className='mt-2 bg-slate-500 text-white p-2 rounded'>
+                  Logout
+                </button>
               </div>
             </div>
-            <form
-              onSubmit={handleSendMessage}
-              className='chat-form flex'>
-              <input
-                type='text'
-                placeholder='Type a message'
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                onKeyPress={handleTyping}
-                className='flex-1 p-2 border rounded'
-              />
-              <button
-                type='submit'
-                className='ml-2 p-2 bg-blue-500 text-white rounded'>
-                Send
-              </button>
-            </form>
-            <div className='message__status mt-2'>
-              <p className='text-gray-500 text-sm'>{typingStatus}</p>
+          ) : (
+            <div>
+              <h3 className='mb-4 text-xl'>Users</h3>
+              {users.length > 0 ? (
+                users.map((item, index) => (
+                  <div
+                    key={index}
+                    onClick={() => handleSelectChat(item)}
+                    className={`p-2 border-b cursor-pointer ${
+                      item.userId === user.uid ? "hidden" : "block"
+                    }`}>
+                    {item.name} ({item.role})
+                    <span
+                      className={`ml-2 inline-block w-2 h-2 rounded-full ${
+                        item.status?.state === "online"
+                          ? "bg-green-500"
+                          : "bg-gray-400"
+                      }`}></span>
+                  </div>
+                ))
+              ) : (
+                <p>No users found.</p>
+              )}
             </div>
-            <button
-              onClick={handleLeaveChat}
-              className='mt-4 bg-blue-500 text-white p-2 rounded'>
-              Leave Chat
-            </button>
-            <button
-              onClick={logout}
-              className='mt-4 bg-slate-500 text-white p-2 rounded'>
-              Logout
-            </button>
-          </>
-        ) : (
-          <div>
-            <h3 className='mb-4 text-xl'>Users</h3>
-            {users.length > 0 ? (
-              users.map((item, index) => (
-                <div
-                  key={index}
-                  onClick={() => handleSelectChat(item)}
-                  className='p-2 border-b cursor-pointer'>
-                  {item.userName} ({item.email})
-                  <span
-                    className={`ml-2 inline-block w-2 h-2 rounded-full ${
-                      item.status?.state === "online"
-                        ? "bg-green-500"
-                        : "bg-gray-400"
-                    }`}></span>
-                </div>
-              ))
-            ) : (
-              <p>No users found.</p>
-            )}
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
